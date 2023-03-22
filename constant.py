67ae70b9f2481654064fda3b41a39656e1fd7d22
Joao Felipe <joao.felipe.alves.m@gmail.com>
2023-03-22 14:12:01 -0300

    Added support to docx files

---
+++ constant.py
@@ -0,0 +1,6 @@
+SUPPORTED_FILES_TYPES = [('Txt files', '*.txt'),
+                         ('Doc files', '*.docx')]
+
+ALL_FILES_TYPES = [('All supported files', '; '.join(y for x, y in SUPPORTED_FILES_TYPES))]
+
+VALID_FILES_TYPES = ALL_FILES_TYPES + SUPPORTED_FILES_TYPES
